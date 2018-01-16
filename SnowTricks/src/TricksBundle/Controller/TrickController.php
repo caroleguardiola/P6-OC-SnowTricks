6625f3d18f05e94e478762bad134219c630188d1
caroleguardiola <carole.guardiola@gmail.com>
2018-01-16 10:42:42 +0100

    Modified routing, view(test) for homepage. Created TrickController and indexAction for homepage.

---
+++ SnowTricks/src/TricksBundle/Controller/TrickController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TricksBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrickController extends Controller
+{
+    public function indexAction()
+    {
+         $content = $this->get('templating')->render('TricksBundle:Trick:index.html.twig');
+    
+    return new Response($content);
+    }
+}
