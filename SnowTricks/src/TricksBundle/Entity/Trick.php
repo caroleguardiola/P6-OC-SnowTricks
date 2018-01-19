abdce0515bc081e63afb02dfb04a3cb35e34b139
caroleguardiola <carole.guardiola@gmail.com>
2018-01-19 12:14:28 +0100

    Created The entity trick with Doctrine.

---
+++ SnowTricks/src/TricksBundle/Entity/Trick.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace TricksBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Trick
+ *
+ * @ORM\Table(name="trick")
+ * @ORM\Entity(repositoryClass="TricksBundle\Repository\TrickRepository")
+ */
+class Trick
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Trick
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Trick
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
+
