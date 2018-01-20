bffa9b22c4d6b736881d37fc0eca7ed203b09561
caroleguardiola <carole.guardiola@gmail.com>
2018-01-20 17:04:45 +0100

    Created the entity Video.

---
+++ SnowTricks/src/TricksBundle/Entity/Video.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace TricksBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Video
+ *
+ * @ORM\Table(name="video")
+ * @ORM\Entity(repositoryClass="TricksBundle\Repository\VideoRepository")
+ */
+class Video
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="TricksBundle\Entity\Trick", cascade={"persist"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $trick;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Video
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set trick
+     *
+     * @param \TricksBundle\Entity\Trick $trick
+     *
+     * @return Video
+     */
+    public function setTrick(\TricksBundle\Entity\Trick $trick)
+    {
+        $this->trick = $trick;
+
+        return $this;
+    }
+
+    /**
+     * Get trick
+     *
+     * @return \TricksBundle\Entity\Trick
+     */
+    public function getTrick()
+    {
+        return $this->trick;
+    }
+}
