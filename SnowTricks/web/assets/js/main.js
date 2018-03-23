945eacb5a380ef605408a31658b4906156fed978
caroleguardiola <carole.guardiola@gmail.com>
2018-03-23 18:54:43 +0100

    Cleaning code js unused and modified animation home and title with jQuery.

--- SnowTricks/web/assets/js/main.js
+++ SnowTricks/web/assets/js/main.js
@@ -31,55 +31,10 @@ jQuery(document).ready(function ($) {
         }
     });
 
-
-
-    /*---------------------------------------------*
-     * WOW
-     ---------------------------------------------*/
-
-    var wow = new WOW({
-        mobile: false // trigger animations on mobile devices (default is true)
-    });
-    wow.init();
-
-// magnificPopup
-
-    $(".popup-img").magnificPopup({
-        type: "image",
-        gallery: {
-            enabled: true
-        }
-    });
-
-    $(".video-link").magnificPopup({
-        type: "iframe",
-        mainClass: "mfp-fade",
-        iframe: {
-  markup: '<div class="mfp-iframe-scaler">'+
-            '<div class="mfp-close"></div>'+
-            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
-
-  patterns: {
-    youtube: {
-      index: "youtube.com/", // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
-
-      id: "v=", // String that splits URL in a two parts, second part should be %id%
-      // Or null - full URL will be returned
-      // Or a function that should return %id%, for example:
-      // id: function(url) { return 'parsed id'; }
-
-      src: "//www.youtube.com/embed/%id%?autoplay=1" // URL that will be set as a source for iframe.
-    }
-    // you may add here more sources
-
-  },
-
-  srcAction: "iframe_src", // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
-}
- 
-    });
-
+// Animation home and titles
+$( document ).ready(function () {
+    $(".animation").fadeIn(6000);
+  });
 
 //---------------------------------------------
 // Scroll Up and Down
@@ -118,7 +73,7 @@ jQuery(document).ready(function ($) {
 $( document ).ready(function () {
     $(".trick_load").slice(0, 15).show();
     var trickslength = $(".trick_load").length;
-      var trickslengthhidden= $(".trick_load:hidden").length;
+    var trickslengthhidden= $(".trick_load:hidden").length;
       if((trickslength-trickslengthhidden) >=15) {
         $(".scrollup").removeClass("hidden")
       }
