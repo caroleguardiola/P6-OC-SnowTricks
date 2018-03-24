6379ed70ab294bdd402acd0ac0f55900918887c8
caroleguardiola <carole.guardiola@gmail.com>
2018-03-24 16:22:05 +0100

    Modified navbar responsive bootsnav.css.

--- SnowTricks/web/assets/js/bootsnav.js
+++ SnowTricks/web/assets/js/bootsnav.js
@@ -1,5 +1,5 @@
 (function ($) {
-	"use strict";
+    "use strict";
     
     var bootsnav = {
         initialize: function() {
@@ -30,22 +30,22 @@
             if( getNav.hasClass("brand-center")){                
                 var postsArr = new Array(),
                     index = $("nav.brand-center"),
-                    $postsList = index.find("ul.navbar-nav");
+                    $postsList = index.find('ul.navbar-nav');
 
                 //Create array of all posts in lists
-                index.find("ul.navbar-nav > li").each(function(){
+                index.find('ul.navbar-nav > li').each(function(){
                     postsArr.push($(this).html());
                 });
                 
                 //Split the array at this point. The original array is altered.
                 var firstList = postsArr.splice(0, Math.round(postsArr.length / 2)),
                     secondList = postsArr,
-                    ListHTML = "";
+                    ListHTML = '';
                 
                 var createHTML = function(list){
-                    ListHTML = "";
+                    ListHTML = '';
                     for (var i = 0; i < list.length; i++) {
-                        ListHTML += "<li>" + list[i] + "</li>"
+                        ListHTML += '<li>' + list[i] + '</li>'
                     }
                 }
                 
@@ -65,7 +65,7 @@
                 index.find("ul.nav.navbar-right").wrap("<div class='col-half right'></div>");
                 
                 //Selection Class
-                index.find("ul.navbar-nav > li").each(function(){ 
+                index.find('ul.navbar-nav > li').each(function(){ 
                     var dropDown = $("ul.dropdown-menu", this),
                         megaMenu = $("ul.megamenu-content", this);
                     dropDown.closest("li").addClass("dropdown");
@@ -111,10 +111,10 @@
             // ------------------------------------------------------------------------------ //
             if( getNav.hasClass("navbar-mobile")){
                 // Add Class to body
-                $(".navbar-collapse").on("shown.bs.collapse", function() {
+                $('.navbar-collapse').on('shown.bs.collapse', function() {
                     $("body").addClass("side-right");
                 });
-                $(".navbar-collapse").on("hide.bs.collapse", function() {
+                $('.navbar-collapse').on('hide.bs.collapse', function() {
                     $("body").removeClass("side-right");
                 });
                 
@@ -224,8 +224,8 @@
                     $(".dropdown-menu", this).removeClass(getOut);
                     
                     // Dropdown Fade Toggle
-                    $("a.dropdown-toggle", this).off("click");
-                    $("a.dropdown-toggle", this).on("click", function (e) {
+                    $("a.dropdown-toggle", this).off('click');
+                    $("a.dropdown-toggle", this).on('click', function (e) {
                         e.stopPropagation();
                         $(this).closest("li.dropdown").find(".dropdown-menu").first().stop().fadeToggle().toggleClass(getIn);
                         $(this).closest("li.dropdown").first().toggleClass("on");                        
@@ -233,9 +233,9 @@
                     });   
                     
                     // Hidden dropdown action
-                    $("li.dropdown", this).each(function () {
+                    $('li.dropdown', this).each(function () {
                         $(this).find(".dropdown-menu").stop().fadeOut();
-                        $(this).on("hidden.bs.dropdown", function () {
+                        $(this).on('hidden.bs.dropdown', function () {
                             $(this).find(".dropdown-menu").stop().fadeOut();
                         });
                         return false;
@@ -262,7 +262,7 @@
                 
                 // Hidden dropdown
                 var cleanOpen = function(){
-                    $("li.dropdown", this).removeClass("on");
+                    $('li.dropdown', this).removeClass("on");
                     $(".dropdown-menu", this).stop().fadeOut();
                     $(".dropdown-menu", this).removeClass(getIn);
                     $(".col-menu", this).removeClass("on");
@@ -484,19 +484,19 @@
         // ------------------------------------------------------------------------------ //
         navbarScrollspy : function(){ 
             var navScrollSpy = $(".navbar-scrollspy"),
-                $body   = $("body"), 
-                getNav = $("nav.navbar.bootsnav"),
+                $body   = $('body'), 
+                getNav = $('nav.navbar.bootsnav'),
                 offset  = getNav.outerHeight();
             
             if( navScrollSpy.length ){
-                $body.scrollspy({target: ".navbar", offset: offset });
+                $body.scrollspy({target: '.navbar', offset: offset });
                 
                 // Animation Scrollspy
-                $(".scroll").on("click", function(event) {
+                $('.scroll').on('click', function(event) {
                     event.preventDefault();
 
                     // Active link
-                    $(".scroll").removeClass("active");
+                    $('.scroll').removeClass("active");
                     $(this).addClass("active");
 
                     // Remove navbar collapse
@@ -510,8 +510,8 @@
 
                     // Scroll
                     var scrollTop = $(window).scrollTop(),
-                        $anchor = $(this).find("a"),
-                        $section = $($anchor.attr("href")).offset().top,
+                        $anchor = $(this).find('a'),
+                        $section = $($anchor.attr('href')).offset().top,
                         $window = $(window).width(),
                         $minusDesktop = getNav.data("minus-value-desktop"),
                         $minusMobile = getNav.data("minus-value-mobile"),
@@ -523,27 +523,27 @@
                         var $position = $section - $minusMobile;
                     }             
                         
-                    $("html, body").stop().animate({
+                    $('html, body').stop().animate({
                         scrollTop: $position
                     }, $speed);
                 });
                 
                 // Activate Navigation
                 var fixSpy = function() {
-                    var data = $body.data("bs.scrollspy");
+                    var data = $body.data('bs.scrollspy');
                     if (data) {
                         offset = getNav.outerHeight();
                         data.options.offset = offset;
-                        $body.data("bs.scrollspy", data);
-                        $body.scrollspy("refresh");
+                        $body.data('bs.scrollspy', data);
+                        $body.scrollspy('refresh');
                     }
                 }
                 
                 // Activate Navigation on resize
                 var resizeTimer;
-                $(window).on("resize", function() {
-                    var resizeTimer = setTimeout(fixSpy, 200);
+                $(window).on('resize', function() {
                     clearTimeout(resizeTimer);
+                    var resizeTimer = setTimeout(fixSpy, 200);
                 });
             }
         }
@@ -575,4 +575,3 @@
     });
     
 }(jQuery));
-
