e92f320f38ef3346de779444f9bfeb8f41481332
caroleguardiola <carole.guardiola@gmail.com>
2018-03-27 08:11:55 +0200

    Modified config for proxy at false. Modified double quotes in single in main.js.

--- SnowTricks/web/assets/js/main.js
+++ SnowTricks/web/assets/js/main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 //Rage Template
 //Designerd by: http://bootstrapthemes.co
@@ -8,23 +8,23 @@ jQuery(document).ready(function ($) {
 //for Preloader
 
     $(window).load(function () {
-        $("#loading").fadeOut(500);
+        $('#loading').fadeOut(500);
     });
 
 
     /*---------------------------------------------*
      * Mobile menu
      ---------------------------------------------*/
-    $("#navbar-menu").find("a[href*='#']:not([href='#'])").click(function () {
-        if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
+    $('#navbar-menu').find('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
             var target = $(this.hash);
-            target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
-                $("html,body").animate({
+                $('html,body').animate({
                     scrollTop: (target.offset().top - 80)
                 }, 1000);
-                if ($(".navbar-toggle").css("display") !== "none") {
-                    $(this).parents(".container").find(".navbar-toggle").trigger("click");
+                if ($('.navbar-toggle').css('display') !== 'none') {
+                    $(this).parents('.container').find('.navbar-toggle').trigger('click');
                 }
                 return false;
             }
@@ -34,14 +34,14 @@ jQuery(document).ready(function ($) {
 
 // Animation home and titles
 $( document ).ready(function () {
-    $(".animation").fadeIn(4000);
+    $('.animation').fadeIn(4000);
   });
 
 
 // magnificPopup
 
-$(".image-magnific-popup").magnificPopup({
-        type: "image",
+$('.image-magnific-popup').magnificPopup({
+        type: 'image',
         gallery: {
             enabled: true
         }
@@ -51,26 +51,26 @@ function extendMagnificIframe(){
 
     var $start = 0;
     var $iframe = {
-        markup: "<div class='mfp-iframe-scaler'>" +
-                "<div class='mfp-close'></div>" +
-                "<iframe class='mfp-iframe' frameborder='0' allowfullscreen></iframe>" +
-                "</div>" +
-                "<div class='mfp-bottom-bar'>" +
-                "<div class='mfp-title'></div>" +
-                "</div>",
+        markup: '<div class="mfp-iframe-scaler">' +
+                '<div class="mfp-close"></div>' +
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                '</div>' +
+                '<div class="mfp-bottom-bar">' +
+                '<div class="mfp-title"></div>' +
+                '</div>',
         patterns: {
             youtube: {
-                index: "youtu", 
+                index: 'youtu', 
                 id: function(url) {   
 
                     var m = url.match( /^.*(?:youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/ );
                     if ( !m || !m[1] ) return null;
 
-                        if(url.indexOf("t=") != - 1){
+                        if(url.indexOf('t=') != - 1){
 
-                            var $split = url.split("t=");
-                            var hms = $split[1].replace("h",":").replace("m",":").replace("s","");
-                            var a = hms.split(":");
+                            var $split = url.split('t=');
+                            var hms = $split[1].replace('h',':').replace('m',':').replace('s','');
+                            var a = hms.split(':');
 
                             if (a.length == 1){
 
@@ -86,25 +86,25 @@ function extendMagnificIframe(){
                             }
                         }                                   
 
-                        var suffix = "?autoplay=0";
+                        var suffix = '?autoplay=0';
 
                         if( $start > 0 ){
 
-                            suffix = "?start=" + $start + "&autoplay=0";
+                            suffix = '?start=' + $start + '&autoplay=0';
                         }
 
                     return m[1] + suffix;
                 },
-                src: "//www.youtube.com/embed/%id%"
+                src: '//www.youtube.com/embed/%id%'
             },
             vimeo: {
-                index: "vimeo.com/", 
+                index: 'vimeo.com/', 
                 id: function(url) {        
                     var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
                     if ( !m || !m[5] ) return null;
                     return m[5];
                 },
-                src: "//player.vimeo.com/video/%id%?autoplay=0"
+                src: '//player.vimeo.com/video/%id%?autoplay=0'
             }
         }
     };
@@ -113,8 +113,8 @@ function extendMagnificIframe(){
 
 }
 
-$(".video-magnific-popup").magnificPopup({
-    type: "iframe",
+$('.video-magnific-popup').magnificPopup({
+    type: 'iframe',
     iframe: extendMagnificIframe()
 });
 
@@ -127,24 +127,24 @@ $(".video-magnific-popup").magnificPopup({
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 400) {
-            $(".scrollup").fadeIn("slow");
+            $('.scrollup').fadeIn('slow');
         } else {
-            $(".scrollup").fadeOut("slow");
+            $('.scrollup').fadeOut('slow');
         }
     });
-    $(".scrollup").click(function () {
-        $("html, body").animate({scrollTop: 0}, 1000);
+    $('.scrollup').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1000);
         return false;
     });
 
 
-    $("a[href^='#']").on("click", function(event) {
+    $('a[href^="#"]').on('click', function(event) {
 
-    var target = $(this.getAttribute("href"));
+    var target = $(this.getAttribute('href'));
 
     if( target.length ) {
         event.preventDefault();
-        $("html, body").stop().animate({
+        $('html, body').stop().animate({
             scrollTop: target.offset().top
         }, 1000);
     }
@@ -156,27 +156,27 @@ $(".video-magnific-popup").magnificPopup({
 //Loading
 
 $( document ).ready(function () {
-    $(".trick_load").slice(0, 15).show();
-    var trickslength = $(".trick_load").length;
-    var trickslengthhidden= $(".trick_load:hidden").length;
+    $('.trick_load').slice(0, 15).show();
+    var trickslength = $('.trick_load').length;
+    var trickslengthhidden= $('.trick_load:hidden').length;
       if((trickslength-trickslengthhidden) >=15) {
-        $(".scrollup").removeClass("hidden")
+        $('.scrollup').removeClass('hidden')
       }
      
     if (trickslengthhidden !== 0) {
-      $("#load").show();
+      $('#load').show();
     }  
 
-    $("#load").on("click", function (e) {
+    $('#load').on('click', function (e) {
       e.preventDefault();
-      $(".trick_load:hidden").slice(0, 5).slideDown();
-      var trickslength = $(".trick_load").length;
-      var trickslengthhidden= $(".trick_load:hidden").length;
+      $('.trick_load:hidden').slice(0, 5).slideDown();
+      var trickslength = $('.trick_load').length;
+      var trickslengthhidden= $('.trick_load:hidden').length;
       if((trickslength-trickslengthhidden) >=15) {
-        $(".scrollup").removeClass("hidden")
+        $('.scrollup').removeClass('hidden')
       }
       if (trickslengthhidden === 0) {
-        $("#load").fadeOut("slow");
+        $('#load').fadeOut('slow');
       }
     });
   });
@@ -190,15 +190,15 @@ $( document ).ready(function () {
 
 $(document).ready(function() {
     // On récupère la balise <div> en question qui contient l'attribut « data-prototype » qui nous intéresse.
-    var $container = $("div#tricksbundle_trick_images");
+    var $container = $('div#tricksbundle_trick_images');
 
     // On définit un compteur unique pour nommer les champs qu'on va ajouter dynamiquement
-    var index = $container.find(":input").length;
+    var index = $container.find(':input').length;
 
     // La fonction qui ajoute un lien de suppression d'une catégorie
     function addDeleteLink($prototype) {
       // Création du lien
-      var $deleteLink = $("<a href='#' class='btn-form'><i class='fa fa-trash-o' aria-hidden='true'></i></a>");
+      var $deleteLink = $('<a href="#" class="btn-form"><i class="fa fa-trash-o" aria-hidden="true"></i></a>');
 
       // Ajout du lien
       $prototype.append($deleteLink);
@@ -214,7 +214,7 @@ $(document).ready(function() {
 
     function addFileReturn($prototype) {
       // Création du lien
-      var $filereturn = $("<p class='file-return'></p>");
+      var $filereturn = $('<p class="file-return"></p>');
 
       // Ajout du lien
       $prototype.append($filereturn);
@@ -225,8 +225,8 @@ $(document).ready(function() {
       // Dans le contenu de l'attribut « data-prototype », on remplace :
       // - le texte "__name__label__" qu'il contient par le label du champ
       // - le texte "__name__" qu'il contient par le numéro du champ
-      var template = $container.attr("data-prototype")
-        .replace(/__name__label__/g, "Image n°" + (index+1))
+      var template = $container.attr('data-prototype')
+        .replace(/__name__label__/g, 'Image n°' + (index+1))
         .replace(/__name__/g,        index)
       ;
 
@@ -246,7 +246,7 @@ $(document).ready(function() {
     }
 
     // On ajoute un nouveau champ à chaque clic sur le lien d'ajout.
-    $("#add_image").click(function(e) {
+    $('#add_image').click(function(e) {
       addImage($container);
 
       e.preventDefault(); // évite qu'un # apparaisse dans l'URL
@@ -264,12 +264,12 @@ $(document).ready(function() {
 //Videos
 
 $(document).ready(function() {
-    var $container = $("div#tricksbundle_trick_videos");
+    var $container = $('div#tricksbundle_trick_videos');
 
-    var index = $container.find(":input").length;
+    var index = $container.find(':input').length;
 
     function addDeleteLink($prototype) {
-      var $deleteLink = $("<a href='#' class='btn-form'><i class='fa fa-trash-o' aria-hidden='true'></i></a>");
+      var $deleteLink = $('<a href="#" class="btn-form"><i class="fa fa-trash-o" aria-hidden="true"></i></a>');
 
       $prototype.append($deleteLink);
 
@@ -282,8 +282,8 @@ $(document).ready(function() {
     }
 
     function addVideo($container) {
-      var template = $container.attr("data-prototype")
-        .replace(/__name__label__/g, "Video n°" + (index+1))
+      var template = $container.attr('data-prototype')
+        .replace(/__name__label__/g, 'Video n°' + (index+1))
         .replace(/__name__/g,        index)
       ;
 
@@ -296,7 +296,7 @@ $(document).ready(function() {
       index++;
     }    
 
-    $("#add_video").click(function(e) {
+    $('#add_video').click(function(e) {
       addVideo($container);
 
       e.preventDefault(); 
@@ -312,27 +312,27 @@ $(document).ready(function() {
 
 //File return
 
-$("#tricksbundle_trick_images").on("change", "input[type='file']", function(){
-        var fileName = this.value.replace(/\\/g, "/").replace(/.*\//, "");
-        $(this).siblings(".file-return").text(fileName);
-        $(this).parent().parent().parent().children(".file-return").text(fileName);
-        $(this).parent().parent().children(".file-return").text(fileName);
+$('#tricksbundle_trick_images').on('change', 'input[type="file"]', function(){
+        var fileName = this.value.replace(/\\/g, '/').replace(/.*\//, '');
+        $(this).siblings('.file-return').text(fileName);
+        $(this).parent().parent().parent().children('.file-return').text(fileName);
+        $(this).parent().parent().children('.file-return').text(fileName);
     });
 
 
-$("#tricksbundle_trick_thumbnail").on("change", "input[type='file']", function(){
-        var fileName = this.value.replace(/\\/g, "/").replace(/.*\//, "");
-        $(this).siblings(".file-return").text(fileName);
-        $(this).parent().parent().parent().children(".file-return").text(fileName);
+$('#tricksbundle_trick_thumbnail').on('change', 'input[type="file"]', function(){
+        var fileName = this.value.replace(/\\/g, '/').replace(/.*\//, '');
+        $(this).siblings('.file-return').text(fileName);
+        $(this).parent().parent().parent().children('.file-return').text(fileName);
     });
 
-$(".input-file-container").on("change", "input[type='file']", function(){
-        var fileName = this.value.replace(/\\/g, "/").replace(/.*\//, "");
-        $(this).siblings(".file-return").text(fileName);
+$('.input-file-container').on('change', 'input[type="file"]', function(){
+        var fileName = this.value.replace(/\\/g, '/').replace(/.*\//, '');
+        $(this).siblings('.file-return').text(fileName);
     });
 
-$("#user_photo").on("change", "input[type='file']", function(){
-        var fileName = this.value.replace(/\\/g, "/").replace(/.*\//, "");
-        $(this).siblings(".file-return").text(fileName);
-        $(this).parent().parent().parent().parent().children(".file-return").text(fileName);
+$('#user_photo').on('change', 'input[type="file"]', function(){
+        var fileName = this.value.replace(/\\/g, '/').replace(/.*\//, '');
+        $(this).siblings('.file-return').text(fileName);
+        $(this).parent().parent().parent().parent().children('.file-return').text(fileName);
     });
